8d44b24fc957aae245f54b1130bc4d980425623b
myshen <myshen@6287ae8f-d90e-4eb8-a75e-95fefb711ae0>
2010-07-01 17:45:41 +0000

    Add some other conversions.
    git-svn-id: http://cchdoimac3.ucsd.edu/svn/webtools/trunk@51 6287ae8f-d90e-4eb8-a75e-95fefb711ae0

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -5,7 +5,7 @@ class ConvertController < ApplicationController
     def index
     end
 
-    def netcdf_to_oceansites_netcdf
+    def ctd_netcdf_to_ctd_oceansites_netcdf
         if params[:file].blank?
             flash[:notice] = 'Please give me a file to convert.'
             render :action => :index
@@ -23,6 +23,56 @@ class ConvertController < ApplicationController
                 file.flush
 
                 send_file(file.path, :filename => 'OS_.nc', :type => 'applications/x-netcdf')
+            rescue
+                raise
+                flash[:notice] = "Error converting file: #{$!}"
+                render :action => :index
+            end
+        end
+    end
+
+    def ctdzip_netcdf_to_ctdzip_oceansites_netcdf
+        if params[:file].blank?
+            flash[:notice] = 'Please give me a file to convert.'
+            render :action => :index
+        else
+            begin
+                tmpfilepath = get_tempfile_path(params[:file])
+                timeseries = params[:timeseries]
+                timeseries = "" unless $ALLOWED_OCEANSITES_TIMESERIES.include?(timeseries)
+
+                cmd = "#{LIBCCHDOBIN}/ctdzip_netcdf_to_ctdzip_oceansites_netcdf.py " + 
+                      "#{tmpfilepath} #{timeseries}"
+                cmdio = IO.popen(cmd, 'rb')
+                file = Tempfile.new('convert')
+                file.write(cmdio.read)
+                file.flush
+
+                send_file(file.path, :filename => 'OS_.zip')
+            rescue
+                raise
+                flash[:notice] = "Error converting file: #{$!}"
+                render :action => :index
+            end
+        end
+    end
+
+    def bottle_exchange_to_kml
+        if params[:file].blank?
+            flash[:notice] = 'Please give me a file to convert.'
+            render :action => :index
+        else
+            begin
+                tmpfilepath = get_tempfile_path(params[:file])
+
+                cmd = "#{LIBCCHDOBIN}/bottle_exchange_to_kml.py " + 
+                      "#{tmpfilepath}"
+                cmdio = IO.popen(cmd, 'rb')
+                file = Tempfile.new('convert')
+                file.write(cmdio.read)
+                file.flush
+
+                send_file(file.path, :filename => 'converted.kml')
             rescue
                 raise
                 flash[:notice] = "Error converting file: #{$!}"

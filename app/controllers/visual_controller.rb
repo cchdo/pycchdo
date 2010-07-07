cac7bf0962620874731f5e713553f85eb5d90013
Matt Shen <synmantics@gmail.com>
2010-07-07 15:59:36 -0700

    Make visual use JSON calls to get a google wire json object

--- app/controllers/visual_controller.rb
+++ app/controllers/visual_controller.rb
@@ -2,20 +2,23 @@ class VisualController < ApplicationController
   layout 'standard'
 
   def index
-    file = params[:file]
-    @filename = nil
-    @wire = nil
-    if file.blank?
-      flash[:notice] = 'Please give me a file to read.'
-    else
-      @filename = file.original_filename || 'data'
-      begin
-        tmpfilepath = get_tempfile_path(file)
-        @wire = `#{LIBCCHDOBIN}/bottle_exchange_to_google_wire.py #{tmpfilepath}`
-        @wire = '{cols:[],rows:[]}' if @wire =~ /Database error/
-      rescue
-        flash[:notice] = "Error parsing file: #{$!}"
-      end
-    end
   end
+
+  def any_to_google_wire
+      file = params[:file]
+      filename = file.original_filename || 'data'
+      begin
+          tmpfilepath = get_tempfile_path(file)
+          logger.debug(tmpfilepath)
+          wire = `#{LIBCCHDOBIN}/any_to_google_wire_json.py #{tmpfilepath}`
+          if wire.include?('Database error') or wire.empty?
+              render :text => "<textarea>null</textarea>", :status => 500
+          else
+              render :text => "<textarea>#{wire}</textarea>"
+          end
+      rescue => e
+          render :text => "<textarea>{\"error\": \"#{e.to_s}\"}</textarea>", :status => 500
+      end
+  end
+
 end

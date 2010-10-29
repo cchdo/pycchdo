a24a081f4e1d123992393500d7a8138d6b0c85a3
Matt Shen <myeeshen@gmail.com>
2010-10-29 12:41:27 -0700

    Fix bad git ignore file keeping config from being tracked.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webtools::Application.config.secret_token = '05cd14c3a19a256294d9f7c7703c6b92110c1580b1b61ff73dd8ed1ff26a59a720587a36bd84d5d3d14120d0e24175f2fe76fc9dd9d855f02ac549c8d941344a'

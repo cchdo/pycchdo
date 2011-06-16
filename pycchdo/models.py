7e949a13c2cb668c32071d435ec1195599cc88c6
Matt Shen <myeeshen@gmail.com>
2011-06-16 12:21:33 -0700

    First draft of model

--- pycchdo/models.py
+++ pycchdo/models.py
@@ -1,42 +1,227 @@
-import transaction
+import datetime
 
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import Unicode
+import pymongo
+import pymongo.objectid
+from pymongo.son_manipulator import SONManipulator
 
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.ext.declarative import declarative_base
+mongo_conn = pymongo.Connection()
+cchdo = mongo_conn.cchdo
 
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 
-from zope.sqlalchemy import ZopeTransactionExtension
+def timestamp():
+    return datetime.datetime.now()
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode(255), unique=True)
-    value = Column(Integer)
+class mongodoc(dict):
+    def copy_keys_from(self, o, keys):
+        if not o:
+            return
+        for key in keys:
+            try:
+                self[key] = o[key]
+            except KeyError:
+                pass
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
 
-def populate():
-    session = DBSession()
-    model = MyModel(name=u'root', value=55)
-    session.add(model)
-    session.flush()
-    transaction.commit()
-    
-def initialize_sql(engine):
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
-    try:
-        populate()
-    except IntegrityError:
-        DBSession.rollback()
+class collectablemongodoc(mongodoc):
+    _mongo_collection = cchdo.collectables
+
+    def from_mongo(self, doc):
+        pass
+
+    def save(self):
+        self._mongo_collection.save(self)
+
+    def remove(self):
+        self._mongo_collection.remove(self['_id'])
+
+    @classmethod
+    def map_mongo(cls, cursor):
+        return [cls().from_mongo(x) for x in cursor]
+
+    @classmethod
+    def all(cls):
+        return cls._mongo_collection.find()
+
+    @classmethod
+    def find(cls, *args, **kwargs):
+        return cls._mongo_collection.find(*args, **kwargs)
+
+
+class Stamp(mongodoc):
+    def __init__(self, person=None):
+        self['timestamp'] = timestamp()
+        # TODO
+        self['person'] = person
+
+
+class Note(mongodoc):
+    def __init__(self, action=None, data_type=None, subject=None, body=None):
+        self['action'] = action
+        self['data_type'] = data_type
+        self['subject'] = subject
+        self['body'] = body
+
+
+class _Change(collectablemongodoc):
+    _mongo_collection = cchdo.changes
+
+    def __init__(self, note=None):
+        self['creation_stamp'] = Stamp()
+        self['pending_stamp'] = None
+        self['judgment_stamp'] = None
+        self['accepted'] = False
+        self['note'] = note
+
+    def from_mongo(self, doc):
+        super(_Change, self).from_mongo(doc)
+        self.copy_keys_from(doc, ('_id', 'creation_stamp',
+                                  'pending_stamp', 'judgment_stamp',
+                                  'accepted', 'note', ))
+        return self
+
+    def is_judged(self):
+        return self['judgment_stamp'] is not None
+
+    def is_acknowledged(self):
+        return self['pending_stamp'] is not None
+
+    def is_accepted(self):
+        return self.is_judged() and self['accepted']
+
+    def is_rejected(self):
+        return self.is_judged() and not self['accepted']
+
+    def accept(self):
+        self['judgment_stamp'] = Stamp()
+        self['accepted'] = True
+        self.save()
+
+    def acknowledge(self):
+        if not self['pending_stamp']:
+            self['pending_stamp'] = Stamp()
+            self.save()
+            return True
+        else:
+            return False
+
+    def reject(self):
+        self['judgment_stamp'] = Stamp()
+        self['accepted'] = False
+        self.save()
+
+
+class _AttrList(list):
+    def __init__(self, L=None):
+        if L:
+            # TODO
+            pass
+
+
+class Attr(_Change):
+    _mongo_collection = cchdo.attrs
+    accepted_value = None
+
+    def __init__(self, key=None, value=None, obj=None, note=None,
+                 deleted=False):
+        super(Attr, self).__init__()
+        self['key'] = key
+        self['value'] = value
+        self['deleted'] = deleted
+        self['obj'] = obj
+        self['note'] = note
+
+    def from_mongo(self, doc):
+        super(Attr, self).from_mongo(doc)
+        self.copy_keys_from(doc, ('key', 'value', 'obj', 'note', 'deleted', ))
+        return self
+
+
+class _Attrs(dict):
+    def __init__(self, obj):
+        self._obj = obj
+
+    def history(self, key=None, **kwargs):
+        query = {'obj': self._obj['_id']}
+        query.update(**kwargs)
+        if key:
+            query['key'] = key
+        return Attr.find(query)
+
+    @property
+    def unacknowledged_changes(self):
+        return Attr.map_mongo(Attr.find({
+            'obj': self._obj['_id'], 'pending_stamp': None,
+            'judgment_stamp': None}).sort(
+            'creation_stamp.timestamp', pymongo.DESCENDING))
+
+    @property
+    def pending_changes(self):
+        return Attr.map_mongo(Attr.find({
+            'obj': self._obj['_id'], 'judgment_stamp': None,
+            'accepted': False}))
+
+    @property
+    def accepted_changes(self):
+        return Attr.map_mongo(Attr.find(
+            {'obj': self._obj['_id'], 'accepted': True}).sort(
+            'judgment_stamp.timestamp', pymongo.DESCENDING))
+
+    def __getitem__(self, key):
+        attrs = self.history(key, accepted=True).sort(
+            'judgment_stamp.timestamp', pymongo.DESCENDING)
+        if attrs.count(True) > 0:
+            attr = Attr.map_mongo(attrs.limit(1))[0]
+            if attr['deleted']:
+                raise KeyError(key)
+            return attr['value']
+        raise KeyError(key)
+
+    def __setitem__(self, key, value, note=None):
+        if type(value) == _AttrList:
+            pass
+        elif type(value) == list:
+            value = _AttrList(value)
+
+        attr = Attr(key, value, self._obj['_id'], note)
+        attr.save()
+
+    def __delitem__(self, key, note=None):
+        attr = Attr(key, None, self._obj['_id'], note, deleted=True)
+        attr.save()
+
+
+class Obj(_Change):
+    _mongo_collection = cchdo.objs
+
+    def __init__(self, doc=None):
+        super(Obj, self).__init__(doc)
+        self['_obj_type'] = type(self).__name__
+        self.copy_keys_from(doc, ('_obj_type', ))
+
+    @property
+    def attrs(self):
+        try:
+            return self['_attrs']
+        except KeyError:
+            self['_attrs'] = _Attrs(self)
+            return self['_attrs']
+
+    def remove(self):
+        super(Obj, self).remove()
+        for attr in Attr.map_mongo(self.attrs.history()):
+            attr.remove()
+
+
+class Data(Attr):
+    pass
+
+
+class Person(Obj):
+    def __init__(self):
+        self['obj_type'] = 'person'
+        self['name_first'] = None
+        self['name_last'] = None
+        self['institution'] = None
+        self['country'] = None
+        self['email'] = None
--- pycchdo/tests.py
+++ pycchdo/tests.py
@@ -1,24 +1,152 @@
 import unittest
+import datetime
+
 from pyramid.config import Configurator
 from pyramid import testing
 
-def _initTestingDB():
-    from sqlalchemy import create_engine
-    from pycchdo.models import initialize_sql
-    session = initialize_sql(create_engine('sqlite://'))
-    return session
 
-class TestMyView(unittest.TestCase):
+class TestModel(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        _initTestingDB()
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_it(self):
-        from pycchdo.views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['root'].name, 'root')
-        self.assertEqual(info['project'], 'pycchdo')
+    def test_new_Change(self):
+        """ Newly created objects have correct values for stamps and notes """
+        from pycchdo.models import _Change
+        before = datetime.datetime.now()
+        change = _Change()
+        after = datetime.datetime.now()
+        self.assertTrue(change['creation_stamp']['timestamp'] >= before)
+        self.assertTrue(change['creation_stamp']['timestamp'] <= after)
+        self.assertTrue(change['pending_stamp'] is None)
+        self.assertTrue(change['judgment_stamp'] is None)
+        self.assertFalse(change['accepted'])
+        self.assertTrue(change['note'] is None)
+
+        from pycchdo.models import Note
+        change1 = _Change(note=Note('action', 'data_type', 'subject', 'body'))
+        self.assertEqual(change1['note']['action'], 'action')
+        self.assertEqual(change1['note']['data_type'], 'data_type')
+        self.assertEqual(change1['note']['subject'], 'subject')
+        self.assertEqual(change1['note']['body'], 'body')
+
+    def test_accept_Change(self):
+        """ Acceptance of _Change """
+        from pycchdo.models import _Change
+        change = _Change()
+        change.accept()
+        self.assertTrue(change.is_accepted())
+        self.assertTrue(change['accepted'])
+        change.remove()
+
+    def test_reject_Change(self):
+        """ Rejection of _Change """
+        from pycchdo.models import _Change
+        change = _Change()
+        change.reject()
+        self.assertTrue(change.is_rejected())
+        self.assertFalse(change['accepted'])
+        change.remove()
+
+    def test_acknowledge_Change(self):
+        """ Acknowledgement of _Change """
+        from pycchdo.models import _Change
+        change = _Change()
+        change.acknowledge()
+        self.assertTrue(change.is_acknowledged())
+        self.assertFalse(change['accepted'])
+        change.remove()
+
+    def test_get_Obj_Attrs(self):
+        """ Obj should always return an _Attrs dict """
+        from pycchdo.models import Obj, _Attrs
+        obj = Obj()
+        attrs = obj.attrs
+        self.assertTrue(isinstance(attrs, _Attrs))
+
+    def test_Attrs_get_scalar(self):
+        """ Getting a scalar value in _Attr should return the latest accepted
+        value.
+        """
+        from pycchdo.models import Obj, Attr
+        obj = Obj()
+        obj.save()
+
+        attrs = obj.attrs
+        key = 'a'
+
+        attrs[key] = '0'
+        attrs[key] = '1'
+        attrs[key] = '2'
+
+        self.assertRaises(KeyError, lambda: attrs[key])
+        Attr.map_mongo(attrs.history(key, value='1'))[0].accept()
+        self.assertEquals(attrs[key], '1')
+        Attr.map_mongo(attrs.history(key, value='2'))[0].accept()
+        self.assertEquals(attrs[key], '2')
+        Attr.map_mongo(attrs.history(key, value='0'))[0].accept()
+        self.assertEquals(attrs[key], '0')
+        del attrs[key]
+        attrs.unacknowledged_changes[0].accept()
+        self.assertRaises(KeyError, lambda: attrs[key])
+
+        obj.remove()
+
+    def test_Attrs_set_scalar(self):
+        """ Setting a scalar value in _Attr should create a new Attr.
+        The key value pair should not appear in _Attr until accepted.
+        The latest accepted key value pair should be the value.
+        
+        """
+        from pycchdo.models import Obj, Attr
+        obj = Obj()
+        obj.save()
+        attrs = obj.attrs
+        key = 'a'
+        value = '0'
+        attrs[key] = value
+        self.assertRaises(KeyError, lambda: attrs[key])
+        history = attrs.history(key)
+        last_attr = Attr.map_mongo(history)[0]
+        self.assertEquals(last_attr['value'], value)
+        last_attr.accept()
+        self.assertEquals(attrs[key], value)
+
+        value1 = '1'
+        attrs[key] = value1
+        self.assertEquals(attrs[key], value)
+        attrs.unacknowledged_changes[0].accept()
+        self.assertEquals(attrs[key], value1)
+
+        del attrs[key]
+        self.assertEquals(attrs[key], value1)
+        attrs.unacknowledged_changes[0].accept()
+        self.assertRaises(KeyError, lambda: attrs[key])
+
+        obj.remove()
+
+    def test_new_Obj(self):
+        """ New Objs are instances of _Change """
+        from pycchdo.models import Obj, _Change
+        obj = Obj()
+        self.assertTrue(isinstance(obj, _Change))
+        self.assertEqual(obj['_obj_type'], 'Obj')
+
+    def test_remove_Obj(self):
+        """ Removing an Obj also removes all Attrs it is associated with. """
+        from pycchdo.models import Obj, Attr
+        obj = Obj()
+        obj.save()
+        attr = Attr('a', '0', obj['_id'])
+        attr.save()
+        self.assertEqual(Attr.map_mongo(Attr.find({'obj': obj['_id']}))[0]['_id'], attr['_id'])
+        obj.remove()
+        self.assertEqual(Attr.find({'obj': obj['_id']}).count(True), 0)
+
+    def test_new_Attr(self):
+        """ New Attrs are instances of _Change """
+        from pycchdo.models import Attr, _Change
+        attr = Attr()
+        self.assertTrue(isinstance(attr, _Change))

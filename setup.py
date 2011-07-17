3c39d1a385c44baf0ec033baf2906f23bda16b26
Matt Shen <myeeshen@gmail.com>
2011-07-17 13:39:32 -0700

    Shift tests into a test package

--- setup.py
+++ setup.py
@@ -9,10 +9,8 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
-    'SQLAlchemy',
     'transaction',
     'repoze.tm2>=1.0b1', # default_commit_veto
-    'zope.sqlalchemy',
     'WebError',
     ]
 

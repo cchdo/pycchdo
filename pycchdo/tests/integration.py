13c36270573a552242822143d811dbf301095f46
Matt Shen <myeeshen@gmail.com>
2011-07-17 14:31:57 -0700

    Rename test_views module to integration

---
+++ pycchdo/tests/integration.py
@@ -0,0 +1,12 @@
+import unittest
+import datetime
+
+from pycchdo.config import Configurator
+from pyramid import testing
+
+class ViewTests(unittest.TestCase):
+  def setUp(self):
+    self.config = testing.setUp()
+
+  def tearDown(self):
+    testing.tearDown()

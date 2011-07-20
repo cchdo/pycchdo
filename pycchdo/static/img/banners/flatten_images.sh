4c73b7ccdcd94ea158489fc8b9585f472afba25e
Matt Shen <myeeshen@gmail.com>
2011-07-20 15:25:42 -0700

    Start banner roll on a rosette image instead of ice

--- pycchdo/static/img/banners/flatten_images.sh
+++ pycchdo/static/img/banners/flatten_images.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Converts all banner*.jpg images into a banner.jpg, avertical strip of images
 # that can be scrolled through.
+
+# Remove the original flattened banner. Otherwise montage does recursion.
+rm banner.jpg
 montage -geometry 1024x125+0+0 -tile 1 -adjoin banner*.jpg banner.jpg

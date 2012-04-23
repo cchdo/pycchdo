13fef7f8c4d682cfbed347ece28f05990d4b97e6
Matt Shen <myeeshen@gmail.com>
2012-04-23 15:10:13 -0700

    Correct submission ambiguity for Argo

--- pycchdo/views/submit.py
+++ pycchdo/views/submit.py
@@ -9,6 +9,80 @@ from pycchdo.views.session import require_signin
 from . import *
 
 
+def _send_confirmation(request, d):
+        cchdo_email = 'cchdo@ucsd.edu'
+        recipients = [
+            request.registry.settings.get('submission_confirmation_recipient',
+                                          request.user.email), cchdo_email]
+
+        d['user_name'] = request.user.full_name()
+        d['file_noun'] = h.whtext.plural(
+            len(d['file_names']), 'file', 'files', False)
+
+        if d['public_status'] == 'public':
+            d['public_description'] = 'will be public.'
+        elif d['public_status'] == 'non_public':
+            d['public_description'] = 'will *not* be public.'
+        elif d['public_status'] == 'non_public_argo':
+            d['public_description'] = \
+                'will be available for use exclusively by the Argo program.'
+        d['file_list'] = '\n'.join(d['file_names'])
+        d['actions'] = '\n'.join(d['action_list'])
+        # TODO
+
+        body_parts = []
+        body = """\
+Dear %(name)s:
+
+Thank you for your submission to the CCHDO.
+
+This is an automated confirmation. However, replying to the sender will reach 
+all senior CCHDO staff.
+
+Your submitted %(file_noun)s:
+%(file_list)s
+
+%(public_description)s.
+
+The following actions were specified:
+%(actions)s
+
+Additional information collected with your submission:
+""".format(d)
+        if d['institution']:
+            body_parts.append('Institution: ' + d['institution'])
+        if d['country']:
+            body_parts.append('Country: ' + d['country'])
+        if d['identifier'] or d['woce_line']:
+            parts = []
+            if d['identifier']:
+                parts.append('ExpoCode: ' + d['identifier'])
+            if d['woce_line']:
+                parts.append('Line: ' + d['woce_line'])
+            body_parts.append(' '.join(parts))
+        if d['ship']:
+            body_parts.append('Ship: ' + d['ship'])
+        if d['cruise_dates']:
+            body_parts.append('Dates: ' + d['cruise_dates'])
+        if d['notes']:
+            body_parts.append('Notes: ' + d['notes'])
+        body_parts.append('\nThank you again for your submission.')
+        body = '\n'.join(body_parts)
+
+        message = Message(
+            subject="[CCHDO] Submission by %(name)s: %(id)s".format(
+                name=d['user_name'],
+                id=' '.join([d['woce_line'], d['identifier']])),
+            sender=cchdo_email,
+            recipients=recipients,
+            body=body,
+        )
+        # XXX actually send the message!
+        print str(message)
+        #mailer = get_mailer(request)
+        #mailer.send(message)
+
+
 def submit(request):
     method = _http_method(request)
     if method == 'GET':
@@ -20,34 +94,27 @@ def submit(request):
     elif method == 'POST':
         if not h.has_edit(request):
             return HTTPUnauthorized()
-        name = request.params.get('name', None)
-        institution = request.params.get('institution', None)
-        country = request.params.get('country', None)
-        email = request.params.get('email', None)
 
-        identifier = request.params.get('identifier', None)
-        woce_line = request.params.get('woce_line', None)
-        ship = request.params.get('ship', None)
-        cruise_dates = request.params.get('cruise_dates', None)
-        notes = request.params.get('notes', None)
+        d = {}
+        d['name'] = request.params.get('name', None)
+        d['institution'] = request.params.get('institution', None)
+        d['country'] = request.params.get('country', None)
+        d['email'] = request.params.get('email', None)
 
-        h.form_entered(request, 'identifier', identifier)
-        h.form_entered(request, 'woce_line', woce_line)
-        h.form_entered(request, 'ship', ship)
-        h.form_entered(request, 'cruise_dates', cruise_dates)
-        h.form_entered(request, 'notes', notes)
+        d['identifier'] = request.params.get('identifier', None)
+        d['woce_line'] = request.params.get('woce_line', None)
+        d['ship'] = request.params.get('ship', None)
+        d['cruise_dates'] = request.params.get('cruise_dates', None)
+        d['notes'] = request.params.get('notes', None)
 
-        type_merge_data = request.params.get('type_merge_data', None)
-        type_place_online = request.params.get('type_place_online', None)
-        type_update_params = request.params.get('type_update_params', None)
-        type_argo = request.params.get('type_argo', None)
-        public = request.params.get('public', 'nonpublic')
+        d['type_merge_data'] = request.params.get('type_merge_data', None)
+        d['type_place_online'] = request.params.get('type_place_online', None)
+        d['type_update_params'] = request.params.get('type_update_params', None)
+        d['public_status'] = request.params.get('public', 'nonpublic')
 
-        h.form_entered(request, 'type_merge_data', type_merge_data)
-        h.form_entered(request, 'type_place_online', type_place_online)
-        h.form_entered(request, 'type_update_params', type_update_params)
-        h.form_entered(request, 'type_argo', type_argo)
-        h.form_entered(request, 'nonpublic', public == 'nonpublic')
+        # Persist in session for form errors
+        for k, v in d.items():
+            h.form_entered(request, k, v)
 
         files = []
         file_keys = filter(lambda k: k.startswith('files'),
@@ -59,10 +126,12 @@ def submit(request):
                 f.filename
                 f.file
             except AttributeError:
+                # TODO handle if files don't have names...shouldn't be possible?
                 continue
             files.append(f)
 
-        h.form_entered(request, 'files', [file.filename for file in files])
+        d['file_names'] = [file.filename for file in files]
+        h.form_entered(request, 'files', d['file_names'])
 
         if len(files) < 1:
             request.response.status = 400
@@ -71,80 +140,41 @@ def submit(request):
             return {}
 
         action_list = []
-        if type_merge_data:
+        if d['type_merge_data']:
             action_list.append('merge_data')
-        if type_place_online:
+        if d['type_place_online']:
             action_list.append('place_online')
-        if type_update_params:
+        if d['type_update_params']:
             action_list.append('update_params')
-        if type_argo:
+        if d['public_status'] == 'nonpublic_argo':
             action_list.append('argo')
+        d['action_list'] = action_list
 
         user = request.user
+        submissions = []
         for file in files:
             s = models.Submission(user)
-            s.save()
-            s.set_accept('expocode', identifier, user)
-            s.set_accept('line', woce_line, user)
-            s.set_accept('ship_name', ship, user)
-            s.set_accept('cruise_date', cruise_dates, user)
-            s.set_accept('action', action_list, user)
-            s.set_accept('public', public, user)
-            s.add_note(models.Note(user, notes).save())
-            s.store_file(file)
+        #    s.save()
+        #    s.set_accept('expocode', d['identifier'], user)
+        #    s.set_accept('line', d['woce_line'], user)
+        #    s.set_accept('ship_name', d['ship'], user)
+        #    s.set_accept('cruise_date', d['cruise_dates'], user)
+        #    s.set_accept('action', d['action_list'], user)
+        #    s.set_accept('public', d['public'] == 'public', user)
+        #    s.add_note(models.Note(user, d['notes']).save())
+        #    s.store_file(file)
+            submissions.append(s)
             # TODO record submitter useragent and ip
+
         # TODO ensure safety of data records and notify users to email
         # cberysgo@ucsd.edu in case of failure
 
-        mailer = get_mailer(request)
+        _send_confirmation(request, d)
 
-        file_noun = 'file'
-        if len(files) != 1:
-            file_noun = 'files'
-
-        message = Message(
-            subject="[CCHDO] Submission by %(name)s: %(id)s".format(
-                name=user.full_name(), id=[line, expocode].join(' ')),
-            sender="cchdo@ucsd.edu",
-            # TODO XXX this needs to be cchdo.
-            recipients=["synmantics+test@gmail.com"],
-            body="""\
-Dear %(name)s:
-
-Thank you for your submission to the CCHDO.
-
-This is an automated confirmation. However, replying to the sender will reach all senior CCHDO staff.
-
-Your submitted %(file_noun)s:
-%(files)s
-
-will be %(public)s.
-
-The following actions were specified:
-%(actions)s
-
-Further information collected with your submission:
-Institution: %(institution)s
-Country: %(country)s
-ExpoCode: %(expocode)s Line: %(line)
-Ship: %(ship)s
-Dates: %(cruise_date)s
-Notes: %(notes)s
-
-Thank you again for your submission.
-""".format(name=user.full_name(), file_noun=file_noun, files='\n'.join(files),
-           public=public, actions='\n'.join(action_list), institution='',
-           country='', expocode=identifier, line=woce_line, ship=ship, 
-           cruise_date=cruise_dates, notes=notes)
-        )
-        print message
-        #mailer.send(message)
-
-        sample_submission = models.Submission(request.user)
-        files = []
-
+        sample_submission = submissions[0]
         return render_to_response(
             'pycchdo:templates/submit_confirmation.jinja2',
-            {'files': files, 'submission': sample_submission}, request=request)
+            {'files': files, 'file_names': d['file_names'],
+             'submission': sample_submission}, request=request)
     return HTTPBadRequest()
 

99d922cdec96f3908572aacd2683e4c08fec5e66
Matt Shen <myeeshen@gmail.com>
2013-12-31 10:42:29 -0800

    Remove old jquery 1.4.4 references

---
+++ pycchdo/static/js/submit.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  var files = $('#dfiles');
+  var remove_button = $('<input type="button" title="Remove" value="Remove">');
+  var fsel= files.find('p :file');
+  var file_list = $('<ol></ol>').appendTo(files);
+
+  function renumber() {
+    file_list.find(':file').each(function (i, x) {
+      $(this).attr('name', 'files[' + i + ']');
+    });
+  }
+
+  function remove() {
+    $(this).parent().hide('fast', function () {
+      $(this).remove();
+      renumber();
+    });
+  }
+
+  function noop(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  files.on('change', 'p :file', function () {
+    if ($(this).val() == null || $(this).val() == '') {
+      return;
+    }
+
+    var f = $(this);
+    var p = f.parent();
+    var c = f.clone();
+    f.click(noop).prop('disabled', true);
+    file_list.prepend(
+      $('<li></li>').append(f).
+        append(remove_button.clone().click(remove)).
+        hide().show('fast'));
+    p.append(c);
+    renumber();
+  });
+});

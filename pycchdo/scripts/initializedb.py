e77c0e70a862d7757fed4a0f8be297b837c73c43
Matt Shen <myeeshen@gmail.com>
2014-07-20 12:03:18 -0700

    Upgrade database initialization script

--- pycchdo/scripts/initializedb.py
+++ pycchdo/scripts/initializedb.py
@@ -1,14 +1,16 @@
-from urlparse import urlparse
+import sys
+import os
+from contextlib import closing
+from getpass import getpass
+from urllib import quote
+from urlparse import urlsplit, urlunsplit, SplitResult
 from subprocess import Popen, PIPE
 from argparse import ArgumentParser
-from StringIO import StringIO
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
-from pyramid.paster import (
-    get_appsettings,
-    setup_logging,
-    )
+from pyramid.paster import get_appsettings, setup_logging
 
 from pycchdo.models.serial import reset_database, Meta
 
@@ -19,10 +21,108 @@ argparser.add_argument(
     help='Skips resetting and just creates new schema')
 argparser.add_argument(
     '--full-reset', action='store_true', default=False,
-    help='Whether to drop the database first')
+    help='Drop the database and recreate')
+argparser.add_argument(
+    '--reset-db', action='store_true', default=False,
+    help='Drop all tables and recreate. Does not drop the database.')
+argparser.add_argument(
+    '--superuser', type=str, default='postgres',
+    help='The adminstrative user with the power to create databases for others.'
+         '(default: postgres)')
+argparser.add_argument(
+    '--template-db', type=str, default='template1',
+    help='A template database to log into temporarily while making changes.')
 argparser.add_argument(
     'config_uri', type=str, nargs='?', default='development.ini',
     help='(default: development.ini)')
+
+
+def sql_check_user(owner):
+    return "SELECT 1 FROM pg_roles WHERE rolname={0!r}".format(owner)
+
+
+def sql_check_database(dbname):
+    return 'SELECT 1 FROM pg_database WHERE datname={0!r}'.format(dbname)
+
+
+def sql_create_user(user, password):
+    return "CREATE USER {0} CREATEDB ENCRYPTED PASSWORD {1!r}".format(user, password)
+
+
+def sql_drop(dbname, owner):
+    return "DROP DATABASE IF EXISTS {dbname};".format(dbname=dbname)
+
+
+def sql_create(dbname, owner):
+    return "\
+CREATE DATABASE {dbname} OWNER {owner} TEMPLATE template_postgis;""".format(
+        dbname=dbname, owner=owner)
+
+
+def sql_grant(owner):
+    tables = [
+        'geography_columns', 'geometry_columns', 'spatial_ref_sys',
+        #'raster_columns', 'raster_overviews',
+    ] 
+    sql = ""
+    for table in tables:
+        sql += (
+            'GRANT select, insert, update, delete ON TABLE public.{table} '
+            'TO {{owner}};\n').format(table=table)
+    return sql.format(owner=owner)
+
+
+def credentials(engine_url):
+    o = urlsplit(engine_url)
+
+    host = o.hostname
+    port = str(o.port or 5432)
+    dbname = o.path[1:]
+    owner = o.username or 'pycchdo'
+    password = o.password
+    assert password is not None
+
+    return host, port, dbname, owner, password, o
+
+
+def drop_and_create_db(args, engine_url):
+    host, port, dbname, owner, password, o = credentials(engine_url)
+
+    try:
+        password = os.environ['PGPASSWORD']
+    except KeyError:
+        password = getpass('Password for {0}: '.format(args.superuser))
+    new_split = o._replace(
+        netloc='{0}:{1}@{2}:{3}'.format(
+            args.superuser, quote(password), o.hostname, o.port),
+        path=args.template_db)
+    superuser_url = urlunsplit(new_split)
+    engine = create_engine(superuser_url)
+    with closing(engine.connect()) as conn:
+        # ensure owner exists
+        print 'checking for user {0}...'.format(owner)
+        with closing(conn.execute(sql_check_user(owner))) as result:
+            if not result.first():
+                print 'creating user {0}...'.format(owner)
+                with closing(conn.execute(
+                        sql_create_user(owner, password))) as result:
+                    pass
+
+        print 're-creating database...'
+        # These operations cannot be done inside a transaction. Commit the open
+        # transaction first.
+        conn.execute("commit")
+        conn.execute(sql_drop(dbname, owner))
+        conn.execute("commit")
+        conn.execute(sql_create(dbname, owner))
+    print 'granting...'
+    new_split = new_split._replace(path=dbname)
+    superuser_url = urlunsplit(new_split)
+    engine = create_engine(superuser_url)
+    with closing(engine.connect()) as conn:
+        sql = sql_grant(owner)
+        with closing(conn.execute(sql)) as result:
+            pass
 
 
 def main():
@@ -30,47 +130,22 @@ def main():
 
     setup_logging(args.config_uri)
     settings = get_appsettings(args.config_uri + '#pycchdo')
+    engine_url = settings['sqlalchemy.url']
 
-    if args.full_reset:
-        url = settings['sqlalchemy.url']
-        o = urlparse(url)
+    # Check if database exists
+    db_missing = False
+    engine = create_engine(engine_url)
+    try:
+        engine.connect().close()
+    except OperationalError as err:
+        db_missing = True
 
-        dbname = o.path[1:]
-        owner = 'pycchdo'
+    if args.full_reset or db_missing:
+        drop_and_create_db(args, engine_url)
 
-        psql_script = """\
-DROP DATABASE IF EXISTS {dbname};
-CREATE DATABASE {dbname} TEMPLATE template_postgis;
-ALTER DATABASE {dbname} OWNER TO {owner};
-\connect {dbname};
-"""
-
-        tables = [
-            'geography_columns', 'geometry_columns', 'spatial_ref_sys',
-            #'raster_columns', 'raster_overviews',
-        ] 
-        for table in tables:
-            psql_script += (
-                'GRANT select, insert, update, delete ON TABLE public.{table} '
-                'TO {{owner}};\n').format(table=table)
-
-        psql_stdin = psql_script.format(dbname=dbname, owner=owner)
-
+    if args.full_reset or db_missing or args.reset_db:
         print 'resetting database...'
-        username = 'postgres'
-        cmd = ['psql',
-                '-h', o.hostname, '-p', str(o.port or 5432),
-                '-U', username,
-                '--set', 'ON_ERROR_STOP=on',
-                'postgres']
-        print ' '.join(cmd)
-        psql_proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = psql_proc.communicate(input=psql_stdin)
-        if 'ERROR' in stderr:
-            raise ValueError(stderr)
-
-    engine = engine_from_config(settings)
-    if args.full_reset:
         reset_database(engine)
-    if args.create_additional:
+    else:
+        print 'creating additional...'
         Meta.create_all(engine)
